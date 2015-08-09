fb7944f2aa1837b924017730b06c97207f72bd44
Damien <damien@dam.io>
2015-08-09 23:29:17 +0200

    add import script

---
+++ import.sh
@@ -0,0 +1,10 @@
+rm -rf ../neo/data/graph.db
+../neo/bin/neo4j-import \
+--into ../neo/data/graph.db \
+--id-type string \
+--nodes:Post csvs/posts.csv \
+--nodes:User csvs/users.csv \
+--nodes:Tag csvs/tags.csv \
+--relationships:PARENT_OF csvs/posts_rel.csv \
+--relationships:HAS_TAG csvs/tags_posts_rel.csv \
+--relationships:POSTED csvs/users_posts_rel.csv
