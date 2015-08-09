3a84ab5d5450b55dfd05451d6325d47a46f9afa9
Damien <damien@dam.io>
2015-08-09 23:24:42 +0200

    add tags + users: working really well!

--- to_csv.py
+++ to_csv.py
@@ -12,11 +12,21 @@ file = join(PATH,'Posts.xml')
 def clean(x):
     return x.replace('\n','').replace('\r','').replace('"','')
 
-spamWriter = csv.writer(open('csvs/posts.csv', 'w'))
-spamWriter_rels = csv.writer(open('csvs/posts_rel.csv', 'w'))
+posts = csv.writer(open('csvs/posts.csv', 'w'))
+posts_rel = csv.writer(open('csvs/posts_rel.csv', 'w'))
+users = csv.writer(open('csvs/users.csv', 'w'))
+users_posts_rel = csv.writer(open('csvs/users_posts_rel.csv', 'w'))
+tags = csv.writer(open('csvs/tags.csv', 'w'))
+tags_posts_rel = csv.writer(open('csvs/tags_posts_rel.csv', 'w'))
 
-spamWriter.writerow(['postId:ID(Post)', 'userId', 'title', 'body'])
-spamWriter_rels.writerow([':START_ID(Post)', ':END_ID(Post)'])
+posts.writerow(['postId:ID(Post)', 'title', 'body'])
+posts_rel.writerow([':START_ID(Post)', ':END_ID(Post)'])
+
+users.writerow(['userId:ID(User)', 'name'])
+users_posts_rel.writerow([':START_ID(User)', ':END_ID(Post)'])
+
+tags.writerow(['tagId:ID(Tag)'])
+tags_posts_rel.writerow([':START_ID(Post)', ':END_ID(Tag)'])
 
 for i, line in enumerate(open(file)):
     line = line.strip()
@@ -24,16 +34,65 @@ for i, line in enumerate(open(file)):
         if line.startswith("<row"):
             el = xmltodict.parse(line)['row']
             el = replace_keys(el)
-            spamWriter.writerow([
-                el['id'], el.get('owneruserid'),
+            posts.writerow([
+                el['id'],
                 clean(el.get('title','')),
                 clean(el.get('body',''))
             ])
             if el.get('parentid'):
-                spamWriter_rels.writerow([el['parentid'],el['id']])
+                posts_rel.writerow([el['parentid'],el['id']])
+            if el.get('owneruserid'):
+                users_posts_rel.writerow([el['owneruserid'],el['id']])
+            if el.get('tags'):
+                eltags = [x.replace('<','') for x in el.get('tags').split('>')]
+                for tag in [x for x in eltags if x]:
+                    tags_posts_rel.writerow([el['id'],tag])
     except Exception as e:
-        print('x')
+        print('x',e)
+    if i % 5000 == 0:
+        print('.',end='')
+    if i > 10000: #only a small sample
+        break
+
+print(i,'posts ok')
+
+file = join(PATH,'Users.xml')
+
+for i, line in enumerate(open(file)):
+    line = line.strip()
+    try:
+        if line.startswith("<row"):
+            el = xmltodict.parse(line)['row']
+            el = replace_keys(el)
+            users.writerow([
+                el['id'],
+                clean(el.get('displayname','')),
+            ])
+    except Exception as e:
+        print('x',e)
     if i % 5000 == 0:
         print('.',end='')
     if i > 1000: #only a small sample
         break
+
+print(i,'users ok')
+
+file = join(PATH,'Tags.xml')
+
+for i, line in enumerate(open(file)):
+    line = line.strip()
+    try:
+        if line.startswith("<row"):
+            el = xmltodict.parse(line)['row']
+            el = replace_keys(el)
+            tags.writerow([
+                el['tagname'],
+            ])
+    except Exception as e:
+        print('x',e)
+    if i % 5000 == 0:
+        print('.',end='')
+    if i > 1000: #only a small sample
+        break
+
+print(i,'tags ok')
